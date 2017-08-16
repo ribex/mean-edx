d25bb2fd26fcdc74284c97fd5b06ce0f63aeb73e
Rebecca Wagaman <ribexy@gmail.com>
2017-08-16 21:09:53 +0000

    Add comments

--- chapters/chapter1/ex3/index.js
+++ chapters/chapter1/ex3/index.js
@@ -1,18 +1,24 @@
 var mongodb = require('mongodb');
 
+// connection string
 var uri = 'mongodb://localhost:27017/example';
+
+// mongo client helper
+// error and db are both potentially null
 mongodb.MongoClient.connect(uri, function(error, db) {
     if (error) {
         console.log(error);
         process.exit(1);
     }
     
+    // else if no error, insert a document, and also create the db 'example' if it did not exist 
     db.collection('sample').insert({ x: 1 }, function(error, result) {
         if (error) {
             console.log(error);
             process.exit(1);
         }
-        
+        // find() returns a cursor, not a set of documents
+        // chain the .toArray() function to return documents
         db.collection('sample').find().toArray(function(error, docs) {
             if (error) {
                 console.log(error);
